65d7e14ff33f5086d461e684ba2afe84bafde8c9
Goryudyuma <umaretekyoumade@gmail.com>
2020-08-02 08:09:46 +0900

    firebaseの追加

---
+++ frontend/public/init-firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA-DWOLWcqAB4YJlwOlseYbi684m2jaVD0",
+    authDomain: "route-063-jp.firebaseapp.com",
+    databaseURL: "https://route-063-jp.firebaseio.com",
+    projectId: "route-063-jp",
+    storageBucket: "route-063-jp.appspot.com",
+    messagingSenderId: "1013476788170",
+    appId: "1:1013476788170:web:8571cd2bec858443427133",
+    measurementId: "G-T0X4VT3Y60"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
