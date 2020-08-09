39aad459467547f5e2504679b48697550c298b0a
Goryudyuma <umaretekyoumade@gmail.com>
2020-08-10 01:13:22 +0900

    buckendの指定

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,5 +6,17 @@ provider "google" {
 
 terraform {
   required_version = "0.12.29"
+  backend "remote" {
+    organization = "route-063-jp"
+
+    workspaces {
+      name = "route"
+    }
+  }
 }
 
+resource "null_resource" "terraform-github-actions" {
+  triggers = {
+    value = "This resource was created using GitHub Actions!"
+  }
+}
